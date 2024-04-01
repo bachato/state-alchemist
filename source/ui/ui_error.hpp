adc46fab1bf2dffdfd3fba6e2d84c8d7a77bcef9
George Tiersma <gtiersma@juno.com>
2024-03-31 21:38:25 -0700

    Created separate UI for error handling
    Also improved error handling in general to be capable of catching all errors.

--- source/ui/ui_error.hpp
+++ source/ui/ui_error.hpp
@@ -1,33 +1,22 @@
 #include <tesla.hpp>    // The Tesla Header
 
-#include <dmntcht.h>
-#include "ui_error.hpp"
-#include "../controller.hpp"
+class GuiError : public tsl::Gui {
+  private:
+    std::string message;
 
-class GuiGroups : public tsl::Gui {
   public:
-    GuiGroups() { }
+    GuiError(std::string message) { this->message = message; }
 
     // Called when this Gui gets loaded to create the UI
     // Allocate all elements on the heap. libtesla will make sure to clean them up when not needed anymore
     virtual tsl::elm::Element* createUI() override {
         // A OverlayFrame is the base element every overlay consists of. This will draw the default Title and Subtitle.
         // If you need more information in the header or want to change it's look, use a HeaderOverlayFrame.
-        auto frame = new tsl::elm::OverlayFrame("The Mod Alchemist", "Mod Groups");
-
-        try {
-          DmntCheatProcessMetadata metadata;
-          dmntchtGetCheatProcessMetadata(&metadata);
-          Controller controller = Controller(metadata.title_id);
-
-          auto list = controller.loadGroups();
-          frame->setContent(list);
-          return frame;
-        } catch (std::filesystem::filesystem_error &e) {
-          tsl::changeTo<GuiError>(std::string("Filesystem error: ") + e.what());
-        } catch (std::exception &e) {
-          tsl::changeTo<GuiError>(e.what());
-        }
+        auto frame = new tsl::elm::OverlayFrame("The Mod Alchemist", "Error");
+        
+        auto uiMessage = new tsl::elm::CategoryHeader(this->message);
+        frame->setContent(uiMessage);
+        return frame;
     }
 
     // Called once every frame to update values
